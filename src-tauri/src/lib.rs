73a27f1a302c8962533943fb5bb4c9892ba88fb6
2301_79117301 <2301_79117301@noreply.gitcode.com>
2026-01-19 22:05:07 +0800

    modified:   src-tauri/src/lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -33,16 +33,10 @@ pub fn check<T, E: Debug>(r: Result<T, E>) -> T {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() -> Result<(), anyhow::Error> {
     let builder = tauri::Builder::default().plugin(tauri_plugin_os::init());
-
-    #[cfg(target_os = "android")]
+    //mobile only
+    #[cfg(any(target_os = "android", target_os = "ios"))]
     {
         builder = builder.plugin(tauri_plugin_barcode_scanner::init());
-    }
-
-    #[cfg(not(target_os = "android"))]
-    {
-        // 对于非 Android 平台，我们不添加条码扫描插件
-        // 因为插件仅在移动平台上可用
     }
 
     builder
@@ -51,73 +45,78 @@ pub fn run() -> Result<(), anyhow::Error> {
         .setup(|app| {
             print!("setting up");
             let apphandle = app.handle().clone();
+            //Desktop only
+            #[cfg(not(any(target_os = "android", target_os = "ios")))]
+            {
+                let quit_i = check(MenuItem::with_id(
+                    &apphandle,
+                    "quit",
+                    "Quit",
+                    true,
+                    None::<&str>,
+                ));
+                let show_i = check(MenuItem::with_id(
+                    &apphandle,
+                    "show",
+                    "Show",
+                    true,
+                    None::<&str>,
+                ));
+                let menu = check(Menu::with_items(&apphandle, &[&quit_i, &show_i])); //窗口上侧菜单
 
-            let quit_i = check(MenuItem::with_id(
-                &apphandle,
-                "quit",
-                "Quit",
-                true,
-                None::<&str>,
-            ));
-            let show_i = check(MenuItem::with_id(
-                &apphandle,
-                "show",
-                "Show",
-                true,
-                None::<&str>,
-            ));
-            let menu = check(Menu::with_items(&apphandle, &[&quit_i, &show_i]));
+                let _tray: tauri::tray::TrayIcon = check(
+                    TrayIconBuilder::new()
+                        .on_menu_event(|apphandle, event| match event.id.as_ref() {
+                            "quit" => {
+                                println!("quit menu item was clicked");
+                                apphandle.exit(0);
+                            }
+                            "show" => {
+                                println!("show menu item was clicked");
+                                let _ = apphandle.get_webview_window("main").unwrap().show();
+                            }
+                            _ => {
+                                println!("menu item {:?} not handled", event.id);
+                            }
+                        })
+                        .menu(&menu)
+                        .show_menu_on_left_click(true)
+                        .build(&apphandle), //系统托盘
+                );
+                let window = apphandle.get_webview_window("main");
+                if let Some(window) = window {
+                    if cfg!(debug_assertions) {
+                        if let Ok(url) = window.url() {
+                            println!(">>> Tauri will load: {}", url);
+                        }
+                    }
+                } else {
+                    eprintln!("Error: Could not get main window");
+                }
+                match app.handle().cli().matches() {
+                    //命令行参数解析段 todo 简单自己托管sever服务：群和接受信息服务
+                    // `matches` here is a Struct with { args, subcommand }.
+                    // `args` is `HashMap<String, ArgData>` where `ArgData` is a struct with { value, occurrences }.
+                    // `subcommand` is `Option<Box<SubcommandMatches>>` where `SubcommandMatches` is a struct with { name, matches }.
+                    Ok(matches) => {
+                        #[cfg(debug_assertions)]
+                        {
+                            println!("matches=>{:?}", matches);
+                            matches
+                                .args
+                                .iter()
+                                .for_each(|(key, value)| println!("{}=>{:?}", key, value));
+                        }
+                        if let Some(arg_data) = matches.args.get("server") {
+                            if &arg_data.value == true {
+                                println!("server mode!");
+                            }
+                        }
+                    }
+                    Err(_) => {}
+                }
+            }
 
-            let _tray: tauri::tray::TrayIcon = check(
-                TrayIconBuilder::new()
-                    .on_menu_event(|apphandle, event| match event.id.as_ref() {
-                        "quit" => {
-                            println!("quit menu item was clicked");
-                            apphandle.exit(0);
-                        }
-                        "show" => {
-                            println!("show menu item was clicked");
-                            let _ = apphandle.get_webview_window("main").unwrap().show();
-                        }
-                        _ => {
-                            println!("menu item {:?} not handled", event.id);
-                        }
-                    })
-                    .menu(&menu)
-                    .show_menu_on_left_click(true)
-                    .build(&apphandle),
-            );
-            let window = apphandle.get_webview_window("main");
-            if let Some(window) = window {
-                if cfg!(debug_assertions) {
-                    if let Ok(url) = window.url() {
-                        println!(">>> Tauri will load: {}", url);
-                    }
-                }
-            } else {
-                eprintln!("Error: Could not get main window");
-            }
-            match app.handle().cli().matches() {
-                // `matches` here is a Struct with { args, subcommand }.
-                // `args` is `HashMap<String, ArgData>` where `ArgData` is a struct with { value, occurrences }.
-                // `subcommand` is `Option<Box<SubcommandMatches>>` where `SubcommandMatches` is a struct with { name, matches }.
-                Ok(matches) => {
-                    #[cfg(debug_assertions)]
-                    {
-                        println!("matches=>{:?}", matches);
-                        matches
-                            .args
-                            .iter()
-                            .for_each(|(key, value)| println!("{}=>{:?}", key, value));
-                    }
-                    if let Some(arg_data) = matches.args.get("server") {
-                        if &arg_data.value == true {
-                            println!("server mode!");
-                        }
-                    }
-                }
-                Err(_) => {}
-            }
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![greet, exit_app]) // 注册命令
